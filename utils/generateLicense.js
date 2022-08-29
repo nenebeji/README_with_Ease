f751b840050a1aae79d78e44a365edf87dffe548
nenebeji <105363883+nenebeji@users.noreply.github.com>
2022-08-29 14:47:07 +0100

    added badge function

--- utils/generateLicense.js
+++ utils/generateLicense.js
@@ -1,6 +1,23 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// TODO: Create a function that returns a license badge and link based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const renderLicenseBadgeLink = (license) => {choices.map(choice => {
+  if('APACHE 2.0 License'){
+    return licensebadge = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+  }
+  else if('The MIT License'){
+    return licensebadge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+  }
+  else if('GNU GPL v3'){
+    return licensebadge = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+  }
+  else if('Boost Software License 1.0'){
+    return  licensebadge = `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`
+  }
+  else if('None'){
+    return licensebadge = {}
+  }
+})
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -18,7 +35,6 @@ function renderLicenseSection(license) {}
 // }
 
 module.exports = {
-  renderLicenseBadge,
-  renderLicenseLink,
+  renderLicenseBadgeLink,
   renderLicenseSection,
 }
